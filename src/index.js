de804b4e3145829ef8d4afe35b6e70a663b82d29
Raghulrv36 <raghulvenkatachalam23@gmail.com>
2024-09-21 13:08:42 +0530

    update project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from "react"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./style.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function ChangeName()
+{
+    var [myname,setmyname] = useState(0)
+
+    function change()
+    {
+        setmyname(
+            Math.floor(Math.random()*100+1)
+        )
+    }
+
+    return(
+        <div className="container">
+            <h1>{myname}</h1>
+            <button className="btn" onClick={change}>Change</button>
+        </div>
+    )
+}
+
+root.render(<ChangeName></ChangeName>)
